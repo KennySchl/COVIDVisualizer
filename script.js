793cd1b722af5a742a853fbe3ea9786385c473d0
Kenneth Schlappkohl <kschlappkohl@gmail.com>
2021-04-28 14:19:17 -1000

    Add bar graph

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let canvas = d3.select("#canvas");
-let bars = d3.select("#bars");
+let bars = d3v5.select("#bars");
 let tooltip = d3.select("#tooltip");
 let sideInfo = d3.select("#legend-container");
 let chosenMap;
@@ -13,6 +13,10 @@ document.querySelectorAll('input[name="map-filter"]').forEach((elem) => {
     chosenMap = e.target.value;
     console.log(chosenMap);
     d3.selectAll("path").remove();
+    let barGraph = document.getElementById("bars");
+    while (barGraph.hasChildNodes()) {
+      barGraph.removeChild(barGraph.lastChild);
+    }
     drawMap(usMap, usData);
     console.log(usData);
     drawBarGraph(usData);
@@ -20,21 +24,20 @@ document.querySelectorAll('input[name="map-filter"]').forEach((elem) => {
 });
 
 const drawBarGraph = (data) => {
-
   const width = 900;
   const height = 450;
   const margin = { top: 50, bottom: 50, left: 50, right: 50 };
 
-  const svg = d3v5
-    .select("#d3-container")
-    .append("svg")
-    .attr("width", width - margin.left - margin.right)
-    .attr("height", height - margin.top - margin.bottom)
-    .attr("viewBox", [0, 0, width, height]);
+  // const svg = d3v5
+  //   .select("#d3-container")
+  //   .append("svg")
+  //   .attr("width", width - margin.left - margin.right)
+  //   .attr("height", height - margin.top - margin.bottom)
+  //   .attr("viewBox", [0, 0, width, height]);
 
   const x = d3v5
     .scaleBand()
-    .domain(d3.range(Object.values(data).length))
+    .domain(d3v5.range(Object.values(data).length))
     .range([margin.left, width - margin.right])
     .padding(0.1);
 
@@ -43,7 +46,7 @@ const drawBarGraph = (data) => {
     .domain([0, 10000])
     .range([height - margin.bottom, margin.top]);
 
-  svg
+  bars
     .append("g")
     .attr("fill", "royalblue")
     .selectAll("rect")
@@ -58,19 +61,19 @@ const drawBarGraph = (data) => {
 
   function yAxis(g) {
     g.attr("transform", `translate(${margin.left}, 0)`)
-      .call(d3.axisLeft(y).ticks(null, data.format))
+      .call(d3v5.axisLeft(y).ticks(null, data.format))
       .attr("font-size", "20px");
   }
 
   function xAxis(g) {
     g.attr("transform", `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom(x).tickFormat((i) => data[i].name))
+      .call(d3v5.axisBottom(x).tickFormat((i) => data[i].county))
       .attr("font-size", "20px");
   }
 
-  svg.append("g").call(xAxis);
-  svg.append("g").call(yAxis);
-  svg.node();
+  bars.append("g").call(xAxis);
+  bars.append("g").call(yAxis);
+  bars.node();
 };
 
 const drawMap = (us, data) => {
